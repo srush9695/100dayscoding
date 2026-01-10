8a96eca4b891859bed55105bd46579c5ff5031ee
srush9695 <patil8694srush@gmail.com>
2026-01-10 21:25:43 +0530

    Create solvemefirst.py

---
+++ solvemefirst.py
@@ -0,0 +1,12 @@
+
+#Complete the function  to compute the sum of two integers.
+
+# Constraints
+# 1<=a,b<=100
+def solveMeFirst(a,b):
+     return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
